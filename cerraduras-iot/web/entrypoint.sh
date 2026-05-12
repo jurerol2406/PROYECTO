9bfa3792b7750700c8966a49ef1046922a49ac59
jurerol2406 <jurerol2406@ieszaidinvergeles.org>
2026-05-12 11:17:51 +0200

    Update entrypoint.sh

--- cerraduras-iot/web/entrypoint.sh
+++ cerraduras-iot/web/entrypoint.sh
@@ -2,9 +2,11 @@
 set -e
 
 mkdir -p /var/lib/smarthome
-touch /var/lib/smarthome/smarthome.db
 chown -R www-data:www-data /var/lib/smarthome
 chmod 775 /var/lib/smarthome
-chmod 664 /var/lib/smarthome/smarthome.db
+
+[ -f /var/lib/smarthome/smarthome.db ] \
+    && chown www-data:www-data /var/lib/smarthome/smarthome.db \
+    && chmod 664 /var/lib/smarthome/smarthome.db
 
 exec apache2-foreground
